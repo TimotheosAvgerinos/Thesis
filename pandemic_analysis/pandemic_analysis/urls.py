8ff23ce3185fbf36b6da9a25d17a1538cac4b286
Timotheos Avgerinos <timotheosavgerinos@Timotheoss-MacBook-Pro.local>
2025-08-04 09:17:37 +0300

    Implement the frontend homepage

--- pandemic_analysis/pandemic_analysis/urls.py
+++ pandemic_analysis/pandemic_analysis/urls.py
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
     path('api/', include('api.urls')),
 
     # Swagger + Redoc UI
